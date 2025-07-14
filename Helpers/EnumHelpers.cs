20f6e08dbe16a08308ddaffd185bd3e7ad8c6229
atevilson <freitas.atevilson@gmail.com>
2025-07-14 18:10:32 -0300

    add helper para encapsular o enum

---
+++ Helpers/EnumHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp.Helpers
+{
+    public static class EnumHelpers
+    {
+        public static IEnumerable<T> GetValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
